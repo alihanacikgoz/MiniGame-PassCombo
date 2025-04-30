1e61e0b0b038c0af67abbc2408c2eaa1ba194ec9
Alihan AÇIKGÖZ <alihanacikgoz@yahoo.com>
2025-04-30 08:40:04 +0300

    TeamMates now disappear within 2 seconds after they have appeared.

--- Assets/Scripts/Runtime/Core/Controllers/TeamMateController.cs
+++ Assets/Scripts/Runtime/Core/Controllers/TeamMateController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Runtime.Core.Managers;
 using Runtime.ScriptableObjects;
 using Runtime.Signals;
@@ -9,6 +10,12 @@ namespace Runtime.Core.Controllers
     public class TeamMateController : MonoBehaviour
     {
         public PoolItem teammateOptions;
+        
+        private void Start()
+        {
+            StartCoroutine(DisappearTeammate());
+        }
+        
         private void OnCollisionEnter2D(Collision2D other)
         {
             if (other.gameObject.CompareTag("Football"))
@@ -16,6 +23,12 @@ namespace Runtime.Core.Controllers
                 gameObject.SetActive(false);
                 BallActionsSignals.Instance.OnCorrectPassAction?.Invoke(teammateOptions.pointsWillGive);
             }
+        }
+
+        IEnumerator DisappearTeammate()
+        {
+            yield return new WaitForSeconds(2f);
+            gameObject.SetActive(false);
         }
     }
 }
